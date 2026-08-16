acabccd4786eb033ebba8d972c71bac462d7d1de
shlok2505 <141041010+shlok2505@users.noreply.github.com>
2026-08-16 11:30:53 +0530

    Simplify return statement for duplicate check

--- Data Structures & Algorithms/duplicate-integer/submission-0.java
+++ Data Structures & Algorithms/duplicate-integer/submission-0.java
@@ -2,6 +2,6 @@ class Solution {
     public boolean hasDuplicate(int[] nums) {
         HashSet<Integer>set=new HashSet<>();
         for(int x:nums)set.add(x);
-        return (set.size()==nums.length)?false:true;
+        return set.size()==nums.length;
     }
 }
